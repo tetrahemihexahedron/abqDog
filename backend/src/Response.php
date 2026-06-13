43b402cdac59d5e7e6f00080465e8474878fe9ed
Sunny Fawcett <95454232+tetrahemihexahedron@users.noreply.github.com>
2026-06-13 15:55:25 -0600

    Add Response class

---
+++ backend/src/Response.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AbqDog;
+
+final readonly class Response
+{
+    /**
+     * @param array<string, mixed> $payload
+     * @param array<string, string> $headers
+     */
+    public function __construct(
+        public int $status,
+        public array $payload,
+        public array $headers = [],
+    ) {
+    }
+}
